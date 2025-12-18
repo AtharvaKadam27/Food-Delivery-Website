69e19f0e94f633a9fdd997c8a21aebada9999e46
Atharva Kadam <atharvak396@gmail.com>
2025-12-18 18:03:48 +0530

    Modify MongoDB connection string and clean up code
    Updated the MongoDB connection string to a placeholder and removed commented-out code.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,8 @@
 import mongoose from "mongoose";
-// mongodb+srv://atharvak396_db_user:rG6sjMMLd7AqeOd2@cluster1.n3etp0u.mongodb.net/?appName=Cluster1
+
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/mern")
+    .connect("Your mongodb URL")
     .then(() => console.log("DB Connected"));
 };
 
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-//   // Use the environment variable, fallback to local if not found
-//   const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/mern";
-
-//   await mongoose
-//     .connect(dbURI)
-//     .then(() => console.log("DB Connected"))
-//     .catch((err) => console.error("DB Connection Error:", err));
-// };
